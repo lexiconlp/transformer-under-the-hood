37fd4a97703ba7dc20874af5027463294a6b87bb
Jorge Galván <jorge.galvan@lexic.ai>
2023-01-08 19:11:44 +0100

    ✨ Add task 3,4 generation functions
    * Signal dataset ( count letters based on signals )

--- transformer_uth/dataset_generator.py
+++ transformer_uth/dataset_generator.py
@@ -10,6 +10,14 @@ Counting letters
 task2:
 Counting letters + Difference between A & B
 "AABA"    -> "31002"
+
+task3:
+Signal
+"ABBBAA" -> "2"
+
+task4:
+Multiple Signals (3 signals)
+"ABBBAA" -> "211"
 """
 import random
 from collections import Counter
@@ -48,3 +56,34 @@ def _output_task2(input_text: str) -> str:
     a, b, *_ = [int(i) for i in count_letters]
     difference = str(abs(a - b))
     return count_letters + difference
+
+
+def _output_task3(input_text: str) -> str:
+    """Task 3 is count only signal letter.
+
+    * The signal is the first letter,
+    * the output number is how many times appears the signal letter
+
+    example:
+    input  -> "BBAA"
+    output -> "1"
+
+    "B" appears only once
+    """
+    signal_char = input_text[0]
+    return str(input_text[1:].count(signal_char))
+
+
+def _output_task4(input_text: str) -> str:
+    """Task 4 is count only signal letters.
+
+    * Is the same as task 3 but using more signals
+
+    example:
+    input  -> "ABBBAA"
+    output -> "211"
+
+    "A" appears 2 times
+    "B" apperas 1 time
+    """
+    return "".join(str(input_text[3:].count(c)) for c in input_text[:3])
